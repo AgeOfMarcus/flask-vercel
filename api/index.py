4002c0ed808b5375817f715ee7696dab75427c8c
Marcus Weinberger <marcusjw@duck.com>
2023-08-23 11:25:41 -0400

    add imports

--- api/index.py
+++ api/index.py
@@ -1,5 +1,11 @@
-from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from flask import Flask, send_file, redirect as _redirect, request, render_template, jsonify
 from flask_cors import CORS
+from PIL import Image
+import os, uuid, base64, json, io, lzma
 
 app = Flask(__name__)
 CORS(app)
